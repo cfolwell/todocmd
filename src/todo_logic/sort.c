b239ccc18b6240acc0d3d387a183c22021d815d4
wmclallen <wmcallen@icloud.com>
2013-12-22 08:31:07 -0500

    Update of sort.h and sort.c

--- src/todo_logic/sort.c
+++ src/todo_logic/sort.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void sort()
+void sort(char *todo_filename);
 {
+	FILE *todo_file;
+	FILE *tmp_file;
+	char buffer[MAX_ARGS];
+	int cnt = 1;
+	int i = 0;
 
-}
+	todo_file = fopen(todo_filename, "w+");
+	tmp_file = fopen("tmp.txt", "r+");
 
-void count_lines()
-{
+	fprintf(tmp_file, "%d: ", cnt);
+	while((i = fgets(buffer, sizeof(buffer), todo_file) != NULL)) {
+		old = cnt;
+		if(i == 1) cnt++;
 
+		if(cnt > old)
+			fprintf(tmp_file, "%d: ", cnt);
+
+		fprintf(tmp_file, "%s", buffer);
+	}
 }
